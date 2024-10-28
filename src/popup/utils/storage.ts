05117f65656e30acf9b4e5623d87e86313c0df0a
John Datserakis <johndatserakis@gmail.com>
2024-10-27 20:57:37 -0400

    Update build

--- src/popup/utils/storage.ts
+++ src/popup/utils/storage.ts
@@ -1,7 +1,10 @@
 import { get } from './chrome';
 import { Ribbons } from './types';
 
-export const STORAGE_KEY_RIBBONS = 'ribbons';
+/**
+ * Fresh key due to large update. John: 2024-10-27.
+ */
+export const STORAGE_KEY_RIBBONS = 'ribbons-v2';
 export const DEFAULT_RIBBONS: Ribbons = [];
 
 export const getRibbons = async () =>
